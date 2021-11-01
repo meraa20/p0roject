87f400f0275ef87e0c92b7cdc8d824751db306cc
meraa20 <amira335636@gmail.com>
2021-11-01 08:20:43 +0200

    add new folder

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdtr1c++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<fixed<<setprecision(6);
+    double a,b,x,m;
+    cin>>b>>a;
+    x=((a/2)*(a/2))*3.14159265359;
+    cout<<(a*a)-x;
+    return 0;
+
+}
